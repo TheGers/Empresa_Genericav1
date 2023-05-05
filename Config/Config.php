c1099139feaf6749dc212aff3d1a1f41dbbc470f
Gerson Garcia <98490152+TheGers@users.noreply.github.com>
2023-05-04 22:03:15 -0600

    cambio de  config

--- Config/Config.php
+++ Config/Config.php
@@ -1,7 +1,7 @@
 <?php 
  
  //define("BASE_URL", "http://localhost/tienda_virtual/");
- const BASE_URL = "http://localhost/loginfunciona";
+ const BASE_URL = "http://localhost/Empresa_Genericav1";
 
  //Zona horaria
  //date_default_timezone_set('America/Honduras');
